70451e0df8c746bee873d1052d547026ef2fff0c
Luigi Moraes <luigimoraes3@gmail.com>
2022-11-21 21:17:51 -0300

    add Vuetify. started develop round

---
+++ src/components/Round.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <p>{{ countryLeft.name }}</p>
+        <p>{{ countryLeft.goals }}</p>
+      </v-col> x
+      <v-col>
+        <p>{{ countryRight.name }}</p>
+        <p>{{ countryRight.goals }}</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "RoundBox",
+  props: {
+    countryLeft: Object,
+    countryRight: Object
+  }
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <round-box 
+    :countryLeft="cLeft"
+    :countryRight="cRight"
+  />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import RoundBox from '../components/Round'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'HomeView',
+    components: {RoundBox},
+    data: () => {
+      return {
+        cLeft: { name: "Brasil", goals: 14 },
+        cRight: { name: "Alemanha", goals: 0 }
+      }
+    }
   }
-}
 </script>
