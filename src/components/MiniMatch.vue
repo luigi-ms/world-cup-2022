bb78beadaa25ff40eba2e8be165dd09868f2f304
Luigi Moraes <luigimoraes3@gmail.com>
2022-12-06 11:55:55 -0300

    Created mini matches for abstraction purposes. PlayoffsMap must load the file to render the matches.

---
+++ src/components/MiniMatch.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-sheet id="round" elevation="4" rounded>
+    <v-container>
+      <v-row class="d-flex flex-column">
+        <v-col v-if="isNameEmpty()">
+          <v-icon v-if="isNameEmpty()">
+            mdi-flag
+          </v-icon>
+          <span>Team</span>
+        </v-col>
+        <v-col v-else>
+          <v-img
+            class="flag"
+            max-width=30 
+            :src="getFlag(countryLeft)"
+            :alt=countryLeft></v-img>
+          <span>{{ countryLeft }}</span>
+        </v-col>
+        <v-col v-if="isNameEmpty()">
+          <v-icon v-if="isNameEmpty()">
+            mdi-flag
+          </v-icon>
+          <span>Team</span>
+        </v-col>
+        <v-col v-else>
+          <v-img 
+            class="flag"
+            max-width=30 
+            :src="getFlag(countryRight)"
+            :alt=countryRight></v-img>
+          <span>{{ countryRight }}</span>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "MiniMatch",
+  data: () => {
+    return {
+      leftWon: false,
+      rightWon: false,
+      countries: {}
+    }
+  },
+  props: {
+    countryLeft: {
+      type: String,
+      default: "Team"
+    },
+    countryRight: {
+      type: String,
+      default: "Team"
+    }
+  },
+  mounted(){
+    fetch('/data/countries.json')
+      .then(res => res.json())
+      .then(json => this.countries = json)
+      .catch(() => console.error("Error on get countries"));
+  },
+  methods: {
+    getFlag(countryName){
+      const abrev = this.countries[countryName];
+
+      if(abrev){
+        return (abrev !== "ESP") 
+          ? require('../assets/'+abrev+'.png')
+          : require('../assets/'+abrev+'.jpeg');
+      }else{
+        return "";
+      }
+    },
+    isNameEmpty(){ 
+      return this.countryLeft === "" 
+      && this.countryRight === ""; 
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+
+#round {
+  font-family: 'Open Sans', 'Arial', sans-serif;
+  margin-top: 2vh;
+  width: 15vw;
+
+}
+
+.flag { 
+  margin-right: 2vw;
+}
+</style>
--- src/components/PlayoffsMap.vue
+++ src/components/PlayoffsMap.vue
@@ -1,40 +1,110 @@
 <template>
-  <v-container>
-    <v-row justify="center" id="sixteens1">
-      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
-      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
-      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
-      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
+  <v-container id="phases">
+    <v-row justify="center">Oitavas de final</v-row>
+    <v-row>
+      <v-col class="playoffMatch" cols="3">
+        <mini-match 
+          countryLeft="A1" 
+          countryRight="B2" />
+      </v-col>
+      <v-col class="playoffMatch" cols="3">
+        <mini-match 
+          countryLeft="C1" 
+          countryRight="D2" />
+      </v-col>
+      <v-col class="playoffMatch" cols="3">
+        <mini-match 
+          countryLeft="E1" 
+          countryRight="F2" />
+      </v-col>
+      <v-col class="playoffMatch" cols="3">
+        <mini-match 
+          countryLeft="G1" 
+          countryRight="H2" />
+      </v-col>
     </v-row>
-    <v-row justify="center" id="quarters1">
-      <v-col class="playoffMatch" cols="6">PAÍS x PAÍS</v-col> 
-      <v-col class="playoffMatch" cols="6">PAÍS x PAÍS</v-col> 
+    <v-row justify="center">Quartas de final</v-row>
+    <v-row>
+      <v-col class="playoffMatch" cols="6">
+        <mini-match 
+          countryLeft="O1" 
+          countryRight="O2" />
+      </v-col> 
+      <v-col class="playoffMatch" cols="6">
+        <mini-match 
+          countryLeft="O3" 
+          countryRight="O4" />
+      </v-col>
     </v-row>
-    <v-row justify="center" id="semifinals1">
-      <v-col class="playoffMatch" cols="12">PAÍS x PAÍS</v-col> 
+    <v-row justify="center">Semifinais</v-row> 
+    <v-row>
+      <v-col class="playoffMatch" cols="12">
+        <mini-match 
+          countryLeft="Q1" 
+          countryRight="Q2" />
+      </v-col> 
     </v-row>
-    <v-row justify="center" id="finals">
-      <v-col class="playoffMatch" cols="12">PAÍS x PAÍS</v-col> 
+    <v-row justify="center">Final</v-row>
+    <v-row>
+      <v-col class="playoffMatch" cols="12">
+        <mini-match 
+          countryLeft="S1" 
+          countryRight="S2" />
+      </v-col> 
     </v-row>
-    <v-row justify="center" id="semifinals2">
-      <v-col class="playoffMatch" cols="12">PAÍS x PAÍS</v-col> 
+    <v-row>
+      <v-col class="playoffMatch" cols="12">
+        <mini-match 
+          countryLeft="Q3" 
+          countryRight="Q4" />
+      </v-col> 
     </v-row>
-    <v-row justify="center" id="quarters2">
-      <v-col class="playoffMatch" cols="6">PAÍS x PAÍS</v-col>
-      <v-col class="playoffMatch" cols="6">PAÍS x PAÍS</v-col>  
+    <v-row justify="center">Semifinais</v-row> 
+    <v-row>
+      <v-col class="playoffMatch" cols="6">
+        <mini-match 
+          countryLeft="O5" 
+          countryRight="O6" />
+      </v-col>
+      <v-col class="playoffMatch" cols="6">
+        <mini-match 
+          countryLeft="O7" 
+          countryRight="O8" />
+      </v-col>  
     </v-row>
-    <v-row justify="center" id="sixteens2">
-      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
-      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
-      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
-      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
+    <v-row justify="center">Quartas de final</v-row> 
+    <v-row>
+      <v-col class="playoffMatch" cols="3">
+        <mini-match 
+          countryLeft="A2" 
+          countryRight="B1" />
+      </v-col>
+      <v-col class="playoffMatch" cols="3">
+        <mini-match 
+          countryLeft="C2" 
+          countryRight="D1" />
+      </v-col>
+      <v-col class="playoffMatch" cols="3">
+        <mini-match 
+          countryLeft="E2" 
+          countryRight="F1" />
+      </v-col>
+      <v-col class="playoffMatch" cols="3">
+        <mini-match 
+          countryLeft="G2" 
+          countryRight="H1" />
+      </v-col>
     </v-row>
+    <v-row justify="center">Oitavas de final</v-row> 
   </v-container>
 </template>
 
 <script>
+  import MiniMatch from './MiniMatch.vue';
+
   export default {
     name: "PlayoffsMap",
+    components: { MiniMatch },
     data: () => {
       return {};
     }
@@ -42,4 +112,14 @@
 </script>
 
 <style scoped>
+.playoffMatch {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  border: 1px solid blue;
+}
+
+#phases > * {
+  border: 1px solid black;
+}
 </style>
