0ed4a95ca1625050cbfa0bb53913746f3d34847e
Luigi Moraes <luigimoraes3@gmail.com>
2022-12-05 22:13:05 -0300

    Using components for table and playoffs map. start to build the PlayoffsMap

---
+++ src/components/PlayoffsMap.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-row justify="center" id="sixteens1">
+      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
+      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
+      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
+      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
+    </v-row>
+    <v-row justify="center" id="quarters1">
+      <v-col class="playoffMatch" cols="6">PAÍS x PAÍS</v-col> 
+      <v-col class="playoffMatch" cols="6">PAÍS x PAÍS</v-col> 
+    </v-row>
+    <v-row justify="center" id="semifinals1">
+      <v-col class="playoffMatch" cols="12">PAÍS x PAÍS</v-col> 
+    </v-row>
+    <v-row justify="center" id="finals">
+      <v-col class="playoffMatch" cols="12">PAÍS x PAÍS</v-col> 
+    </v-row>
+    <v-row justify="center" id="semifinals2">
+      <v-col class="playoffMatch" cols="12">PAÍS x PAÍS</v-col> 
+    </v-row>
+    <v-row justify="center" id="quarters2">
+      <v-col class="playoffMatch" cols="6">PAÍS x PAÍS</v-col>
+      <v-col class="playoffMatch" cols="6">PAÍS x PAÍS</v-col>  
+    </v-row>
+    <v-row justify="center" id="sixteens2">
+      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
+      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
+      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
+      <v-col class="playoffMatch" cols="3">PAÍS x PAÍS</v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "PlayoffsMap",
+    data: () => {
+      return {};
+    }
+  }
+</script>
+
+<style scoped>
+</style>
--- src/views/MapAndTable.vue
+++ src/views/MapAndTable.vue
@@ -12,58 +12,7 @@
         md="6" 
         lg="2" 
         xl="4">
-        <v-sheet elevation="4" 
-          rounded>
-          <v-simple-table class="table">
-            <template v-slot:default>
-              <thead>
-                <tr>
-                  <th>
-                    <span class="groupHeader groupName">
-                      Grupo {{ group.groupName }}
-                    </span>
-                  </th>
-                </tr>
-                <tr>
-                  <th v-for="header in tableHeader" 
-                    :key="header">
-                    <span class="groupHeader">{{ header }}</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{{ group.first.name }}</td>
-                  <td v-for="(point, i) in group.first.points"
-                    :key="i">
-                    {{ point }}
-                  </td>
-                </tr>
-                <tr>
-                  <td>{{ group.second.name }}</td>
-                  <td v-for="(point, i) in group.second.points"
-                    :key="i">
-                    {{ point }}
-                  </td>
-                </tr>
-                <tr>
-                  <td>{{ group.third.name }}</td>
-                  <td v-for="(point, i) in group.third.points"
-                    :key="i">
-                    {{ point }}
-                  </td>
-                </tr>
-                <tr>
-                  <td>{{ group.fourth.name }}</td>
-                  <td v-for="(point, i) in group.fourth.points"
-                    :key="i">
-                    {{ point }}
-                  </td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
-        </v-sheet>          
+        <points-table :group="group" />
       </v-col> 
     </v-row>
     <v-row>
@@ -73,18 +22,19 @@
     </v-row>
     <v-row>
       <v-col>
-        <v-skeleton-loader type="image"/>
+        <playoffs-map />
       </v-col> 
     </v-row>
   </v-container>
 </template>
 
 <script>
-//import MatchList from '../components/MatchList';
-  
+  import PointsTable from '../components/PointsTable.vue';
+  import PlayoffsMap from '../components/PlayoffsMap.vue';
+
   export default {
     name: 'MapAndTable',
-    //  components: { MatchList },
+    components: { PointsTable, PlayoffsMap },
     data: () => {
       return {
         tableHeader: [],
@@ -105,10 +55,4 @@
 
 <style scoped>
 h1 { text-align: center; }
-
-.table { 
-  background-color: #EDE7F6;
-}
-
-.groupName { font-size: 1rem; }
 </style>
